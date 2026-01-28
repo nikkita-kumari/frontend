ba53d6238a6528106cba1d5437dc1277fabdc16f
nikkita-kumari <nikitakumari1082005@gmail.com>
2026-01-29 01:43:38 +0530

    feat: enhance support and pricing pages with new layouts and content

--- src/landing_page/pricing/PricingPage.jsx
+++ src/landing_page/pricing/PricingPage.jsx
@@ -3,10 +3,12 @@ import Hero from './Hero';
 import Brokerage from './Brokerage';
 // import Navbar from '../Navbar';
 // import Footer from '../Footer';
+import OpenAccount from '../OpenAccount'
 function PricingPage() {
   return ( <>
   
   <Hero/>
+  <OpenAccount/>
   <Brokerage/>
   
   </> );
